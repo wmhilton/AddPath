3cc1270af0931998f4f973195e1ed6cbb1857187
Jason Rowe <jason.rowe@dowjones.com>
2012-04-25 11:43:04 -0500

    initial program

---
+++ Options.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AddPath
+{
+    public class Options
+    {
+        public EnvironmentVariableTarget Target { get; set; }
+        public string PathSegment { get; set; }
+
+        public bool Parse(string[] args)
+        {
+            Target = EnvironmentVariableTarget.Machine;
+
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("-"))
+                {
+                    switch (arg)
+                    {
+                        case "-p":
+                            Target = EnvironmentVariableTarget.Process;
+                            break;
+
+                        case "-m":
+                            Target = EnvironmentVariableTarget.Machine;
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown option: {0}", arg);
+                            return false;
+                    }
+                }
+                else
+                {
+                    if (PathSegment == null)
+                    {
+                        PathSegment = arg;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown parameter: {0}", arg);
+                        return false;
+                    }
+                }
+
+            }
+
+            if(PathSegment == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("    AddPath  [options] [path]");
+            Console.WriteLine();
+            Console.WriteLine("    [options] can be zero or more of:");
+            Console.WriteLine("        -p    only change path in process");
+            Console.WriteLine("        -m    change path on machine is default");
+            Console.WriteLine();
+            Console.WriteLine("    [path] directory you want to add");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AddPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+
+            if (!options.Parse(args))
+            {
+                options.Usage();
+                return;
+            }
+
+            AddPathSegments(options);
+        }
+
+        public static void AddPathSegments(Options options)
+        {
+            try
+            {
+                string allPaths = Environment.GetEnvironmentVariable("PATH", options.Target);
+                if (allPaths != null)
+                    allPaths = options.PathSegment + "; " + allPaths;
+                else
+                    allPaths = options.PathSegment;
+                Environment.SetEnvironmentVariable("PATH", allPaths, options.Target);
+
+                Console.WriteLine("Added path segment: {0}", options.PathSegment);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("could not add path segment: {0} -  error {1}", options.PathSegment, ex);
+            }
+        }
+    }
+}
